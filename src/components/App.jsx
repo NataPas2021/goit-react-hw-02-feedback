7c77fa2f87f3b289e1691c96acb5e28c98bcc470
NataPas2021 <natapas13@gmail.com>
2023-04-12 12:18:03 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import { Component } from "react";
+import { FeedbackTitle } from "./FeedbackTitle/FeedbackTitle";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className='app-container'>
+      <FeedbackTitle title="Please, leave feedback"/>
+      <Statistics good={} neutral={} bad={} total={} positivePercentage={} />
     </div>
   );
+    };
 };
+
---
+++ src/components/FeedbackTitle/FeedbackTitle.jsx
@@ -0,0 +1,5 @@
+export const FeedbackTitle = ({title}) => {
+    return (
+        <h1>{title}</h1>
+    )
+}
