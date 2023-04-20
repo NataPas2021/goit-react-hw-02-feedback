de74a4595e3a6b033ede4654544b5c9a441dedd7
NataPas2021 <natapas13@gmail.com>
2023-04-20 12:25:03 +0300

    next commit

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,14 @@
 export const FeedbackOptions = (options, onLeaveFeedback) => {
     return (
-        <>
-        {options.map(option => {
-         <button onClick={onLeaveFeedback}>Good</button>
-         <button onClick={onLeaveFeedback}>Neutral</button>
-         <button onClick={onLeaveFeedback}>Bad</button>
-        })}
-        
-        </>
-        
+    <>
+    {options.map(({good, neutral, bad}) {
+        return (
+         <button type="button" onClick={onLeaveFeedback}>Good</button>
+         <button type="button" onClick={onLeaveFeedback}>Neutral</button>
+         <button type="button" onClick={onLeaveFeedback}>Bad</button>
+        )
+        })
+    }
+    </>  
     )
 }
